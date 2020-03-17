6cec076911c747b8f68b3cf6d143c471b12af1ee
nineven <nineven@qq.com>
2020-03-17 08:23:07 +0800

    set default app count size 20

--- fir_ser/api/views/apps.py
+++ fir_ser/api/views/apps.py
@@ -23,7 +23,7 @@ REDIS_CONN = redis.Redis(decode_responses=True)
 
 
 class AppsPageNumber(PageNumberPagination):
-    page_size = 10  # 每页显示多少条
+    page_size = 20  # 每页显示多少条
     page_size_query_param = 'size'  # URL中每页显示条数的参数
     page_query_param = 'page'  # URL中页码的参数
     max_page_size = None  # 最大页码数限制
